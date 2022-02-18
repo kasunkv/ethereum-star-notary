71b31a071800778f60832741638002970d57cf32
Kasun Kodagoda <kvkrusl@gmail.com>
2022-02-18 15:52:28 +0530

    Added the code to bring the project to the staring point.

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -2,6 +2,50 @@
 
 pragma solidity >=0.8.10;
 
-contract StarNotary {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract StarNotary is ERC721 {
+
+	struct Star {
+		string name;
+	}
+
+	mapping(uint => Star) public tokenIdToStarInfo;
+	mapping(uint => uint) public starsForSale;
+
+	constructor() ERC721("Star Notary", "SN") {
+
+	}
+
+	function createStar(string memory _name, uint _tokenId) public {
+		Star memory newStar = Star(_name);
+		tokenIdToStarInfo[_tokenId] = newStar;
+
+		_safeMint(msg.sender, _tokenId);
+	}
+
+	function putStarUpForSale(uint _tokenId, uint _price) public {
+		require(ownerOf(_tokenId) == msg.sender, "You can't sell a star that you don't own");
+
+		starsForSale[_tokenId] = _price;
+	}
+
+	function buyStar(uint _tokenId) public payable {
+		require(starsForSale[_tokenId] > 0, "Star is not up for sale");
+
+		uint starCost = starsForSale[_tokenId];
+		address ownerAddress = ownerOf(_tokenId);
+
+		require(msg.value > starCost, "Not enough ether to buy the star");
+
+		// to bypass the checks that are inplace in other transfaer functions, using the _transfer function
+		_transfer(ownerAddress, msg.sender, _tokenId);
+
+		payable(ownerAddress).transfer(starCost);
+
+		if (msg.value > starCost) {
+			payable(msg.sender).transfer(msg.value - starCost);
+		}
+	}
 
 }
--- test/starNotary.spec.js
+++ test/starNotary.spec.js
@@ -12,6 +12,78 @@ contract('StarNotary', async (accounts) => {
 		sut = await StarNotary.deployed();
 	});
 
+	it('Can create a star', async () => {
+		const tokenId = 1;
 
+		await sut.createStar('Awesome Star', tokenId, { from: ownerAccount });
+		const starName = await sut.tokenIdToStarInfo.call(tokenId);
+
+		assert.equal(starName, 'Awesome Star');
+	});
+
+	it('Lets user1 put up their star for sale', async () => {
+		const starId = 2;
+		const starPrice = web3.utils.toWei('0.01', 'ether');
+
+		await sut.createStar('More Awesome Star', starId, { from: firstUserAccount });
+		await sut.putStarUpForSale(starId, starPrice, { from: firstUserAccount });
+
+		const salePrice = await sut.starsForSale.call(starId);
+
+		assert.equal(salePrice, starPrice);
+	});
+
+	it('Lets user1 get the funds after the sale', async () => {
+		const starId = 3;
+		const starPrice = web3.utils.toWei('0.01', 'ether');
+		const balance = web3.utils.toWei('0.05', 'ether');
+
+		await sut.createStar('More More Awesome Star', starId, { from: firstUserAccount });
+		await sut.putStarUpForSale(starId, starPrice, { from: firstUserAccount });
+
+
+		const balanceOfFirstUserBeforeTransaction = await web3.eth.getBalance(firstUserAccount);
+		await sut.buyStar(starId, { from: secondUserAccount, value: balance });
+		const balanceOfFirstUserAfterTransaction = await web3.eth.getBalance(firstUserAccount);
+
+		const value1 = Number(balanceOfFirstUserBeforeTransaction) + Number(starPrice);
+		const value2 = Number(balanceOfFirstUserAfterTransaction);
+
+		assert.equal(value1, value2);
+	});
+
+	it('lets user2 buy a star, if it is put up for sale', async() => {
+    const starId = 4;
+    const starPrice = web3.utils.toWei(".01", "ether");
+    const balance = web3.utils.toWei(".05", "ether");
+
+    await sut.createStar('Super Awesome Star', starId, {from: firstUserAccount});
+    await sut.putStarUpForSale(starId, starPrice, {from: firstUserAccount});
+
+    await sut.buyStar(starId, {from: secondUserAccount, value: balance});
+		const newOwner = await sut.ownerOf.call(starId);
+
+    assert.equal(newOwner, secondUserAccount);
+	});
+
+	it('lets user2 buy a star and decreases its balance in ether', async() => {
+		const starId = 5;
+		const starPrice = web3.utils.toWei(".01", "ether");
+		const balance = web3.utils.toWei(".05", "ether");
+
+		await sut.createStar('Super Super Awesome Star', starId, {from: firstUserAccount});
+		await sut.putStarUpForSale(starId, starPrice, {from: firstUserAccount});
+
+		const balanceOfSecondUserBeforeTransaction = await web3.eth.getBalance(secondUserAccount);
+
+		await sut.buyStar(starId, {from: secondUserAccount, value: balance});
+
+		const balanceAfterSecondUserBuysStar = await web3.eth.getBalance(secondUserAccount);
+
+		const value = Number(balanceOfSecondUserBeforeTransaction) - Number(balanceAfterSecondUserBuysStar);
+
+		assert.isTrue(balanceAfterSecondUserBuysStar < balanceOfSecondUserBeforeTransaction);
+
+	});
 
 });
